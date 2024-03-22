518655463b3d5d2095bd3615aad744f0b655e1e8
SantGT5 <gianspf@gmail.com>
2024-03-22 23:20:59 +0100

    feat: add routes crud

--- router/router.go
+++ router/router.go
@@ -3,15 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
-	// init Router using the default Gin config
-	router := gin.Default()
+	// init Router
+	var router *gin.Engine = gin.Default()
 
-	// defining Route
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// init Routers
+	initializeRoutes(router)
 
-	router.Run(":8080") // listen and serve on 0.0.0.0:8080
+	// listen and serve on 0.0.0.0:8080
+	router.Run(":8080")
 }
