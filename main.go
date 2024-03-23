68c2d2c63a266cad09652ee0c42b438f7997cebb
SantGT5 <gianspf@gmail.com>
2024-03-23 14:37:39 +0100

    feat: schema and custom loggers

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/SantGT5/gopportunities/router"
+import (
+	"github.com/SantGT5/gopportunities/config"
+	"github.com/SantGT5/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
-	//init Routers
+	logger = config.GetLogger("main")
+	// init Configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config init error: %v", err.Error())
+		return
+	}
+
+	// init Routers
 	router.Initialize()
 }
