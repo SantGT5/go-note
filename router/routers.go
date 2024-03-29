bd7b27e2d09ef51a5c15ce7b078151b76d807ab6
SantGT5 <gianspf@gmail.com>
2024-03-29 14:58:50 +0100

    feat: POST /opening

--- router/routers.go
+++ router/routers.go
@@ -6,17 +6,16 @@ import (
 )
 
 func initializeRoutes(router *gin.Engine) {
+	// init handler
+	handler.InitializeHandler()
+
 	var v1 *gin.RouterGroup = router.Group("/api/v1/")
 
 	{
 		v1.GET("/opening", handler.GetOpeningHandler)
-
 		v1.POST("/opening", handler.PostOpeningHandler)
-
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
-
 		v1.PUT("/opening", handler.PutOpeningHandler)
-
 		v1.GET("/openings", handler.GetOpeningsHandler)
 	}
 }
